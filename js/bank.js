0a84cfd00b3afa9519ffceb786ede4fa447182ad
Asfe1 <aarafath172154@bscse.uiu.ac.bd>
2022-02-10 13:48:00 +0600

    index file added

---
+++ js/bank.js
@@ -0,0 +1,11 @@
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const email = document.getElementById('email');
+
+    const password = document.getElementById('password');
+    // email.value = '';
+    // password.value='';
+    if (email.value == 'asfe@gmail.com' && password.value == '1234') {
+        window.location.href = 'banking.html';
+    }
+
+})
